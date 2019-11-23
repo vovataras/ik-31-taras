bdf3fe0da4b6955df3c00096014af58e7ba3af84
Volodya Taras <proger.sdk@gmail.com>
2019-11-23 14:22:33 +0200

    Lab_6: Update .travis.yml

--- lab_5/scripts/travis-build.sh
+++ lab_5/scripts/travis-build.sh
@@ -8,5 +8,5 @@ sleep 120
 kill $PID_test
 kill $PID_run
 make push
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; docker push vovataras/lab4-examples:django-travis; else echo "PR skip deploy"; fi
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; fi
 exit 0
