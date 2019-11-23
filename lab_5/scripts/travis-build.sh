e1194082b68ad286d7e071c1d7fa7fe4eec542fd
Volodya Taras <proger.sdk@gmail.com>
2019-11-23 22:23:04 +0200

    Lab_6: Clean up unnecessary

--- lab_5/scripts/travis-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-make run-back
-sleep 180
-make test-app
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; fi
-make push
-exit 0
