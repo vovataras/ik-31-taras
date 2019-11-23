5f2f3f7c88b1a9cd40e74ece09b8b321bfda0585
Volodya Taras <proger.sdk@gmail.com>
2019-11-23 20:37:59 +0200

    Lab_6: Update .travis.yml, Makefile and script in lab 5

--- lab_5/scripts/travis-build.sh
+++ lab_5/scripts/travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
-make run-back
-make test-app-back
+#make run-back
+make test-app
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; fi
 make push
 exit 0
