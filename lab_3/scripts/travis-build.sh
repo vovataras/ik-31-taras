d0b662a384b5588026d90b01651bfeb9539078d6
Volodya Taras <proger.sdk@gmail.com>
2019-11-23 14:04:21 +0200

    Lab_6: Update travis-build scripts

--- lab_3/scripts/travis-build.sh
+++ lab_3/scripts/travis-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ev
 nohup pipenv run server > ./ci-build.log &
-pipenv run python monitoring.py || true
+PID_run=$!
+pipenv run python monitoring.py &
+PID_test=$!
+sleep 120
+kill $PID_test
+kill $PID_run
 exit 0
--- lab_5/scripts/travis-build.sh
+++ lab_5/scripts/travis-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ev
-make run
-make test-app
+make run &
+PID_run=$!
+make test-app &
+PID_test=$!
+sleep 120
+kill $PID_test
+kill $PID_run
 make push
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; docker push vovataras/lab4-examples:django-travis; else echo "PR skip deploy"; fi
 exit 0
