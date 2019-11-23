4f0bbfb7144471e8f9296b5cf7bf77015e90acac
Volodya Taras <proger.sdk@gmail.com>
2019-11-23 17:57:22 +0200

    Lab_6: Update travis-build script in lab_5

--- lab_5/scripts/travis-build.sh
+++ lab_5/scripts/travis-build.sh
@@ -3,6 +3,6 @@ set -ev
 make run-back
 make test-app-back
 sleep 120
-make push
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; fi
+make push-travis
 exit 0
